28bc4c4d79d68b8ccdcd93f280b1705098267a26
Jaden <veganlife@byui.edu>
2026-03-07 00:52:45 -0800

    Looking good.

--- TGCARDistributedMLTracker/app/src/main/java/com/example/tgcardistributedmltracker/CardDetector.kt
+++ TGCARDistributedMLTracker/app/src/main/java/com/example/tgcardistributedmltracker/CardDetector.kt
@@ -1,5 +1,6 @@
 package com.example.tgcardistributedmltracker
 
+import android.content.Context
 // Core TFLite Interpreter imports
 import org.tensorflow.lite.Interpreter
 //import org.tensorflow.lite.GpuDelegate This will take a few steps
@@ -8,13 +9,14 @@ import org.tensorflow.lite.Interpreter
 import org.tensorflow.lite.support.common.FileUtil
 
 
-class CardDetector {
+class CardDetector(context: Context) {
     val options = Interpreter.Options().apply {
         setNumThreads(4)
         // GPU acceleration:
 //         addDelegate(GpuDelegate())
     }
-    val tfliteModel = FileUtil.loadMappedFile(context(), "/../assets/best_int8.tflite")
+    private val tfliteModel = FileUtil.loadMappedFile(context, "best_int8.tflite")
+
     val interpreter = Interpreter(tfliteModel, options)
 
 }
